a6a708d82a11195973a7baf6efb056f051993dc5
dudikpal <dudikpal@gmail.com>
2020-10-29 19:50:01 +0100

    Book class

---
+++ src/main/java/classsctructureconstructors/Book.java
@@ -0,0 +1,40 @@
+package classsctructureconstructors;
+
+public class Book {
+    private String author;
+    private String title;
+    private String regNumber;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public void register(String regNumber) {
+        setRegNumber(regNumber);
+    }
+}
