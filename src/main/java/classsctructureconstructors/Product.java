304f3e9ab3d6a31f2a13f41fcb25ddb3979eea41
dudikpal <dudikpal@gmail.com>
2020-10-29 20:26:39 +0100

    Product class

---
+++ src/main/java/classsctructureconstructors/Product.java
@@ -0,0 +1,27 @@
+package classsctructureconstructors;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void increasePrice(int rise) {
+        this.price += rise;
+    }
+
+    public void decreasePrice(int rise) {
+        this.price -= rise;
+    }
+}
