60abccb6289fc43239628a20b0a8b9f8e5947dd0
dudikpal <dudikpal@gmail.com>
2020-11-01 00:13:05 +0100

    daysOfWeek

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -1,16 +1,29 @@
 package arrays;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.time.LocalDate.*;
 
 public class ArraysMain {
 
-    String numberOfDaysAsString() {
+    static String numberOfDaysAsString() {
         int [] numberOfDays = new int [12];
         for (int i = 0; i < 12; i++) {
-            numberOfDays[i] = (of(2019, i + 1, 1)).lengthOfMonth();
+            numberOfDays[i] = (of(2020, i + 1, 1)).lengthOfMonth();
         }
-        return numberOfDays.toString();
+        return Arrays.toString(numberOfDays);
+    }
+
+    static List<String> daysOfWeek() {
+        String [] napok = {"hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap"};
+        return Arrays.asList(napok);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfDaysAsString());
+
+        System.out.println(daysOfWeek());
     }
 }
