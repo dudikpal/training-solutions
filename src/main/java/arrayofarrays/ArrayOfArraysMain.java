302af5bbefd4013c3e4691595f9bba6bb6e5ec2e
dudikpal <dudikpal@gmail.com>
2020-10-31 22:28:28 +0100

    triangularMatrix

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -3,7 +3,7 @@ package arrayofarrays;
 import java.util.Scanner;
 
 public class ArrayOfArraysMain {
-    public static int[][] multiplicationTable(int size) {
+    private static int[][] multiplicationTable(int size) {
         int [][] szorzoTabla = new int [size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
@@ -13,7 +13,7 @@ public class ArrayOfArraysMain {
     return szorzoTabla;
     }
 
-    public static void printArrayOfArrays(int[][] a) {
+    private static void printArrayOfArrays(int[][] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 System.out.print(a[i][j] + " ");
@@ -22,10 +22,24 @@ public class ArrayOfArraysMain {
         }
     }
 
+    private static int[][] triangularMatrix(int size) {
+        int [][] triangMatrix = new int [size][];
+        for (int i = 0; i < size; i++) {
+            triangMatrix[i] = new int [i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                triangMatrix[i][j] = i;
+            }
+        }
+        return triangMatrix;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Szorzótábla dimenziója:");
         printArrayOfArrays(multiplicationTable(scanner.nextInt()));
+
+        System.out.println("A háromszögmátrix mérete:");
+        printArrayOfArrays(triangularMatrix(scanner.nextInt()));
     }
 }
