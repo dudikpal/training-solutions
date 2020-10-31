4126b3ce795de7d5006b72ee4e408ca3d604a260
dudikpal <dudikpal@gmail.com>
2020-11-01 00:21:43 +0100

    multiplicationTableAsString

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -21,9 +21,21 @@ public class ArraysMain {
         return Arrays.asList(napok);
     }
 
+    static void multiplicationTableAsString(int size) {
+        int [][] szorzoTabla = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                szorzoTabla[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        System.out.println(Arrays.deepToString(szorzoTabla));
+    }
+
     public static void main(String[] args) {
         System.out.println(numberOfDaysAsString());
 
         System.out.println(daysOfWeek());
+
+        multiplicationTableAsString(5);
     }
 }
