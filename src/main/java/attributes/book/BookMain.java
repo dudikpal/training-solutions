3cf2fbf3fd85865aaeb698f8cb6e7495ee8e880a
dudikpal <dudikpal@gmail.com>
2020-11-03 22:50:22 +0100

    Book class

---
+++ src/main/java/attributes/book/BookMain.java
@@ -0,0 +1,12 @@
+package attributes.book;
+
+public class BookMain {
+
+    public static void main(String[] args) {
+
+        Book book = new Book("Könyv címe");
+        System.out.println(book.getTitle());
+
+    }
+
+}
