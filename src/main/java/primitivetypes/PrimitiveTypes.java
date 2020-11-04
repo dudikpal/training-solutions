ecdb678f6acb91fd5efa4dafdc12380d91ac2693
dudikpal <dudikpal@gmail.com>
2020-11-04 11:17:06 +0100

    Week2 feladat

---
+++ src/main/java/primitivetypes/PrimitiveTypes.java
@@ -0,0 +1,10 @@
+package primitivetypes;
+
+public class PrimitiveTypes {
+
+    public String toBinaryString(int n) {
+        int decNumber = n;
+
+        return Integer.toBinaryString(n);
+    }
+}
---
+++ src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -0,0 +1,13 @@
+package primitivetypes;
+
+public class PrimitiveTypesMain {
+
+    public static void main(String[] args) {
+
+        PrimitiveTypes number = new PrimitiveTypes();
+        int szam = 5;
+
+        System.out.println(number.toBinaryString(szam));
+
+    }
+}
