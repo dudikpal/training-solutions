f0c3cad60d5639eea8a65ff51c4d30633e2967ca
dudikpal <dudikpal@gmail.com>
2020-11-06 10:11:30 +0100

    Week2 languages class

---
+++ src/main/java/typeconversion/whichtype/Type.java
@@ -0,0 +1,24 @@
+package typeconversion.whichtype;
+
+public enum Type {
+
+    BYTE(Byte.MIN_VALUE, Byte.MAX_VALUE),
+    SHORT(Short.MIN_VALUE, Short.MAX_VALUE),
+    INT(Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+    private long min;
+    private long max;
+
+    Type(long min, long max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public long getMin() {
+        return min;
+    }
+
+    public long getMax() {
+        return max;
+    }
+}
--- src/main/java/typeconversion/whichtype/WhichType.java
+++ src/main/java/typeconversion/whichtype/WhichType.java
@@ -1,4 +1,19 @@
 package typeconversion.whichtype;
 
+import com.sun.jdi.Type;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WhichType {
+
+
+
+    public List<Type> whichType(String s) {
+        List<Type> types = new ArrayList<>();
+        /*for (Type type: Type.values()) {
+
+        }*/
+        return null;
+    }
 }
