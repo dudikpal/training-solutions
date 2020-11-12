e759b5c4d85c81dcfd67d936170ac6dfb3473b50
dudikpal <dudikpal@gmail.com>
2020-11-12 15:33:43 +0100

    Refactor week03.PhoneParserTest

--- src/test/java/week03/PhoneParserTest.java
+++ src/test/java/week03/PhoneParserTest.java
@@ -7,9 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PhoneParserTest {
 
     @Test
-    public void testParse() {
-        assertEquals(new Phone("30", "1234567").getNumber(), new PhoneParser().parse( "30-1234567").getNumber());
+    void testPrefix() {
         assertEquals(new Phone("30", "1234567").getPrefix(), new PhoneParser().parse( "30-1234567").getPrefix());
+    }
 
+    @Test
+    void testNumber() {
+        assertEquals(new Phone("30", "1234567").getNumber(), new PhoneParser().parse( "30-1234567").getNumber());
     }
 }
