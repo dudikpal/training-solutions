1d0afbe01626aeaac7d9431b25ecda437509efdd
dudikpal <dudikpal@gmail.com>
2020-11-18 10:39:58 +0100

    week04 Szamkitalalo

---
+++ src/main/java/week04/Szamkitalalo.java
@@ -0,0 +1,29 @@
+package week04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Szamkitalalo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rnd = new Random();
+        int gondoltSzam = rnd.nextInt(100);
+        int szamlalo = 0;
+        System.out.println("Gondoltam egy számra 1 és 100 között. Mi a tipped?");
+
+        while (szamlalo < 6) {
+            int tipp = Integer.parseInt(scanner.nextLine());
+            if (gondoltSzam > tipp) {
+                System.out.println("A tippednél nagyobbra gondoltam.");
+                szamlalo++;
+            } else if (gondoltSzam < tipp) {
+                System.out.println("A tippednél kisebbre gondoltam.");
+                szamlalo++;
+            } else {
+                System.out.println("Gratulálok! Sikerült eltalálnod!");
+                break;
+            }
+        }
+    }
+}
