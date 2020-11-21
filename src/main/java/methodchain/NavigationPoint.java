5220cea4971a7c0933de29b26894a28ef561bd76
dudikpal <dudikpal@gmail.com>
2020-11-21 20:23:16 +0100

    Java SE alapok - Java OO elemek - Attribútumok és metódusok - Metódus hívások láncolása - Robot class

---
+++ src/main/java/methodchain/NavigationPoint.java
@@ -0,0 +1,18 @@
+package methodchain;
+
+public class NavigationPoint {
+
+    private int distance;
+    private int azimut;
+
+    public NavigationPoint(int distance, int azimut) {
+        this.distance = distance;
+        this.azimut = azimut;
+    }
+
+    @Override
+    public String toString() {
+        return "distance: " + distance +
+                " azimut: " + azimut;
+    }
+}
