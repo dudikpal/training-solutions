f1edc476faa6c8613721ffedf7a0b39a87b6fe09
dudikpal <dudikpal@gmail.com>
2020-12-04 10:15:53 +0100

    week06d05

---
+++ src/main/java/week06d05/Bottle.java
@@ -0,0 +1,31 @@
+package week06d05;
+
+public class Bottle {
+
+    private BottleType type;
+    private int filledUntil;
+
+    public Bottle(BottleType type) {
+        this.type = type;
+        filledUntil = 0;
+    }
+
+    public static Bottle of(BottleType bottleType) {
+        return new Bottle(bottleType);
+    }
+
+    public void fill(int fillAmount) {
+        if (filledUntil + fillAmount > type.getMaximumAmount()) {
+            throw new IllegalArgumentException("Overflow error!");
+        }
+        filledUntil += fillAmount;
+    }
+
+    public BottleType getType() {
+        return type;
+    }
+
+    public int getFilledUntil() {
+        return filledUntil;
+    }
+}
