774983fc6bc653101aa2104d38c8738db4753d3f
dudikpal <dudikpal@gmail.com>
2020-12-11 11:17:39 +0100

    week07d05

---
+++ src/test/java/week07d05/SaveInputTest.java
@@ -0,0 +1,37 @@
+package week07d05;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaveInputTest {
+    @TempDir
+    public Path tempDirectory;
+
+    @Test
+    void testCreate() {
+
+        Scanner scanner = new Scanner("aaa\nbbb\nccc");
+        SaveInput si = new SaveInput(scanner);
+        List<String> datas = si.readLine();
+
+        assertEquals(List.of("aaa", "bbb", "ccc"), datas);
+
+    }
+
+    @Test
+    void testWrite() throws IOException {
+        SaveInput si = new SaveInput(new Scanner(System.in));
+
+        Path file = tempDirectory.resolve("alma.txt");
+        System.out.println(file);
+        System.out.println(tempDirectory);
+    }
+
+}
