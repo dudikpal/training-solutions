15de85e17e369247118fe8858ed59ad071298bea
dudikpal <dudikpal@gmail.com>
2021-01-03 02:05:09 +0100

    04 - Java SE haladó - Kollekciók és osztálykönyvtárak - Alapvető algoritmusok - Keresés -  BookArraySearch feladat

---
+++ src/main/java/searching/Book.java
@@ -0,0 +1,50 @@
+package searching;
+
+import java.util.Objects;
+
+public class Book implements Comparable<Book> {
+
+    private int id;
+    private String author;
+    private String title;
+
+    public Book(int id, String author, String title) {
+        this.id = id;
+        this.author = author.toLowerCase();
+        this.title = title.toLowerCase();
+    }
+
+    public Book(String author, String title) {
+        this.author = author.toLowerCase();
+        this.title = title.toLowerCase();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + author + " " + title;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+
+        if (author.compareTo(o.author) != 0) {
+            return author.compareTo(o.author);
+        } else {
+            return title.compareTo(o.title);
+        }
+    }
+
+
+}
