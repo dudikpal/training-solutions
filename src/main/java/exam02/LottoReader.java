c0ddb8594e13f7c26380dceb0229d9a385b9fef1
dudikpal <dudikpal@gmail.com>
2021-01-06 21:04:32 +0100

    Vizsga 02 Lottósorsolás feladat

--- src/main/java/exam02/LottoReader.java
+++ src/main/java/exam02/LottoReader.java
@@ -1,4 +1,29 @@
 package exam02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class LottoReader {
+
+    private int[] number = new int[90];
+
+    public LottoReader(InputStream is) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split(";");
+                for (int i = 11; i < 16; i++) {
+                    number[Integer.parseInt(words[i]) - 1]++;
+                }
+            }
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Cannot read file, " + ioe);
+        }
+    }
+
+    public int getNumber(int number) {
+        return this.number[number - 1];
+    }
 }
