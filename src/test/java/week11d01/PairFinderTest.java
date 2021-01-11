455822d0b389b71c4d11305d6955d6e3b86872d2
dudikpal <dudikpal@gmail.com>
2021-01-11 10:12:03 +0100

    week11d01 PairFinder feladat

---
+++ src/test/java/week11d01/PairFinderTest.java
@@ -0,0 +1,18 @@
+package week11d01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PairFinderTest {
+
+    @Test
+    void testCreate() {
+        PairFinder pairFinder = new PairFinder();
+
+        int[] arr = {7, 1, 5, 7, 3, 3, 5, 7, 6, 7};
+
+        assertEquals(3, pairFinder.findPairs(arr));
+    }
+
+}
