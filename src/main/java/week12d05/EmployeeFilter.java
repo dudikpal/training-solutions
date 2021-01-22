3df6fc907810b4de40e0ab2e27a07dec193c85e9
dudikpal <dudikpal@gmail.com>
2021-01-22 13:12:48 +0100

    week12d05 senior feladat

---
+++ src/main/java/week12d05/EmployeeFilter.java
@@ -0,0 +1,19 @@
+package week12d05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmployeeFilter {
+
+    public List<Employee> countSeniorDevs(List<Employee> employees) {
+        /*List<Employee> developers = new ArrayList<>();
+        for (Employee employee : employees) {
+          if (employee.getSkills().contains("programming") && employee.getSkillLevel() > 2) {
+              developers.add(employee);
+          }
+        }
+        return developers;*/
+        return employees.stream().filter((devs) -> devs.getSkillLevel() > 2 && devs.getSkills().contains("programming")).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/week12d05/EmployeeFilterTest.java
@@ -0,0 +1,23 @@
+package week12d05;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeFilterTest {
+
+    @Test
+    void countSeniorDevs() {
+        EmployeeFilter ef = new EmployeeFilter();
+        List<Employee> employees = List.of(
+                new Employee(35, 4, "Csávó1", List.of("programming", "drinking", "eating")),
+                new Employee(39, 1, "Csávó1", List.of("programming", "drinking", "eating")),
+                new Employee(45, 2, "Csávó1", List.of("sinking", "drinking", "eating")),
+                new Employee(34, 3, "Csávó1", List.of("thinking", "drinking", "eating")),
+                new Employee(25, 4, "Csávó1", List.of("programming", "drinking", "eating"))
+        );
+        assertEquals(2, ef.countSeniorDevs(employees).size());
+    }
+}
