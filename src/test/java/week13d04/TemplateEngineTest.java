c20dad2090694557c9f3fb463ecd19346a916a99
dudikpal <dudikpal@gmail.com>
2021-01-28 15:41:22 +0100

    week13d04

---
+++ src/test/java/week13d04/TemplateEngineTest.java
@@ -0,0 +1,57 @@
+package week13d04;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemplateEngineTest {
+  
+  @TempDir
+  public File folder;
+  
+  @Test
+  void merge() {
+    
+    Map<String, Object> map = new HashMap<>(){{
+      put("nev", "John Doe");
+      put("datum", LocalDate.of(2018, 8, 8));
+      put("osszeg", 10000);
+      put("hatarido", LocalDate.of(2018, 8, 18).toString());
+    }};
+    
+    String template= "Kedves {nev}!\n" +
+                         "Megküldjük önnek a következő esedékes számláját {datum} dátummal,\n" +
+                         "melynek összege: {osszeg} Ft!\n" +
+                         "A fizetési határidő {hatarido}.\n" +
+                         "Üdvözlettel,\n" +
+                         "Ügyfélszolgálat";
+    
+    InputStream is = this.getClass().getResourceAsStream("template.txt");
+    StringReader sr = new StringReader(template);
+    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    StringWriter sw = new StringWriter();
+    BufferedWriter bw = new BufferedWriter(sw);
+    
+    new TemplateEngine().merge(br, map, bw);
+    
+    System.out.println(br.toString());
+    System.out.println(sr.toString());
+    System.out.println(bw.toString());
+    System.out.println(sw.toString());
+    
+    
+    assertEquals("Kedves {nev}!\n" +
+                     "Megküldjük önnek a következő esedékes számláját {datum} dátummal,\n" +
+                     "melynek összege: {osszeg} Ft!\n" +
+                     "A fizetési határidő {hatarido}.\n" +
+                     "Üdvözlettel,\n" +
+                     "Ügyfélszolgálat", sw.toString());
+  }
+}
